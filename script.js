ce061d2c8b7538cd72323a300afbad07a522f18e
Blaze <blaze_awa@163.com>
2025-03-15 14:27:52 +0800

    discord修改

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function link(){
 function copy(){
     var textareaC = document.createElement('textarea');
     textareaC.setAttribute('readonly', 'readonly'); //设置只读属性防止手机上弹出软键盘
-    textareaC.value = 'SQJ#5152';
+    textareaC.value = '_sqj_';
     document.body.appendChild(textareaC); //将textarea添加为body子元素
     textareaC.select();
     var res = document.execCommand('copy');
